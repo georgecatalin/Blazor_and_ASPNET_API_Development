4d21aa07840a24f0b6de6e3a3e14e445fe4e263d
George Calin <george.calin@gmail.com>
2026-08-28 11:57:59 +0300

    added Entity Core to your projects and scaffolding

--- BookStoreApp.API/Program.cs
+++ BookStoreApp.API/Program.cs
@@ -1,8 +1,14 @@
+using BookStoreApp.API.Data;
+using Microsoft.EntityFrameworkCore;
 using Serilog;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connString = builder.Configuration.GetConnectionString("BookStoreDbConnection");
+// Add the DbContext to the service container and configure it to use SQL Server with the connection string from appsettings.json. Do this after you have created the DbContext file and added the necessary NuGet packages for Entity Framework Core and SQL Server. This will allow your application to interact with the database using the BookStoreDbContext class
+builder.Services.AddDbContext<BookStoreDbContext>(options => options.UseSqlServer(connString));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
