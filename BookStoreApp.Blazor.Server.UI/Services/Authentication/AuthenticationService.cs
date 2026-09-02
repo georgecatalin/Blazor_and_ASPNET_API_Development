513f8d745335d826576d67ae3ab63343ece9e279
George Calin <george.calin@gmail.com>
2026-09-02 16:37:07 +0300

    work in progress

---
+++ BookStoreApp.Blazor.Server.UI/Services/Authentication/AuthenticationService.cs
@@ -0,0 +1,22 @@
+﻿using BookStoreApp.Blazor.Server.UI.Services.Base;
+
+namespace BookStoreApp.Blazor.Server.UI.Services.Authentication
+{
+    public class AuthenticationService : IAuthenticationService
+    {
+        private readonly IClient _httpClient;
+        public AuthenticationService(IClient httpClient)
+        {
+            this._httpClient = httpClient;
+        }
+        public Task<bool> AuthenticateAsync(LoginUserDTO userDTO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Logout()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
